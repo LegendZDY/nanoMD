232fee628c31e959014acfe38bedce8f5e834a11
DeYing Zhang <legendzdy@dingtalk.com>
2025-02-14 15:35:53 +0800

    feat: select

--- nanomd/scripts/selectM6A.R
+++ nanomd/scripts/selectM6A.R
@@ -395,6 +395,87 @@ ggplot2::ggsave(paste0(output_path, "/", prefix, "-483.pdf"),
               p2, width=12, height=8)
 ggplot2::ggsave(paste0(output_path, "/", prefix, "-483.tiff"), 
               p2, width=12, height=8)
+
+
+# MAPKAPK2-449
+nc_value <- "NC-3-3"
+si_value <- "si-mettl3-1-6"
+
+prefix <- "MAPKAPK2"
+
+
+sub <- raw_data %>%
+  dplyr::select(all_of(c("x", nc_value, si_value)))
+
+data_subset <- sub %>%
+  tidyr::pivot_longer(cols = c(nc_value, si_value), 
+              names_to = "group", 
+              values_to = "value") %>% 
+  dplyr::mutate(group2 = case_when(
+              grepl("^NC", group) ~ "NC",
+              TRUE ~ "siMettl3"))
+
+bar_data <- as.data.frame(
+  group <- c("NC", "siMettl3")) %>%
+  dplyr::mutate(values = case_when(
+    group == "NC" ~ sub[which(sub[, 2] > 0.2)[1], "x"],
+    group == "siMettl3" ~ sub[which(sub[, 3] > 0.2)[1], "x"]
+  ))
+
+color_set <- c("siMettl3"="#3e7aaf", "NC"="#c37f54")
+
+p <- ggplot2::ggplot(data_subset, aes(x, value, group = group2, color = group2)) +
+  geom_line(linewidth = 2) +
+  scale_color_manual(values = color_set) +  # 使用 scale_color_manual
+  labs(x = "MAPKAPK2 449 site", y = "RT") +
+  theme_bw() +
+  theme(
+    panel.grid = element_blank(),  # 去掉背景网格线
+    legend.title = element_blank(),  # 去掉图例标题
+    legend.position = c(0.98, 0.1),  # 设置图例位置（右下角）
+    legend.justification = c(1, 0),
+    axis.title.x = element_text(size = 20, face = "bold"),  # 横坐标加大且加粗
+    axis.title.y = element_text(size = 20, face = "bold"),  # 纵坐标加大且加粗
+    axis.text.x = element_text(size = 12, face = "bold"),  # 横坐标刻度加粗
+    axis.text.y = element_text(size = 12, face = "bold"),  # 纵坐标刻度加粗
+    legend.text = element_text(size = 12, face = "bold"),  # 图例文字加粗
+    axis.line = element_line(size = 1),  # 坐标轴线条加粗
+    panel.border = element_blank()
+  ) 
+
+bar <- ggplot(bar_data, aes(fill=group, y=values, x=group)) + 
+    geom_bar(position = position_stack(), stat="identity", width = .7) +
+    scale_fill_manual(values=color_set) +
+    geom_text(aes(label = ifelse(group == "siMettl3", "***", ""),
+                vjust = -0.5),
+            size = 6,
+            color = "black") +
+    guides(fill = guide_legend(title = NULL)) +
+    xlab("") +
+    ylab(bquote(bold(qPCR) ~ C[T])) +
+    theme_bw() +
+    theme(
+    panel.grid = element_blank(),  # 去掉背景网格线
+    legend.title = element_blank(),  # 去掉图例标题
+    axis.title.x = element_text(size = 20, face = "bold"),  # 横坐标加大且加粗
+    axis.title.y = element_text(size = 20, face = "bold"),  # 纵坐标加大且加粗
+    axis.text.x = element_text(size = 20, face = "bold"),  # 横坐标刻度加粗
+    axis.text.y = element_text(size = 12, face = "bold"),  # 纵坐标刻度加粗
+    legend.text = element_text(size = 12, face = "bold"),  # 图例文字加粗
+    axis.line = element_line(size = 1),  # 坐标轴线条加粗
+    panel.border = element_blank()
+    )
+
+p2 <- p + draw_plot(bar, x = 0, y = 0.06, hjust = 0.27, vjust = 0, width = 50, height = 0.74, scale = 0.4)
+p2
+ggplot2::ggsave(paste0(output_path, "/", prefix, "-449.png"), 
+              p2, width=12, height=8)
+ggplot2::ggsave(paste0(output_path, "/", prefix, "-449.pdf"), 
+              p2, width=12, height=8)
+ggplot2::ggsave(paste0(output_path, "/", prefix, "-449.tiff"), 
+              p2, width=12, height=8)
+
+
 # p1 <- plot_grid(p, bar, labels = c("A", "B"), rel_widths = c(2, 1), label_size = 12)
 
 # ggplot2::ggsave(paste0(output_path, "/", prefix, ".png"), 
