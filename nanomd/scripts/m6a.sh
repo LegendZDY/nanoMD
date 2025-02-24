84c9c1e25818317f189878261bf7171086c7d668
DeYing Zhang <legendzdy@dingtalk.com>
2025-02-24 07:47:06 +0000

    feat: split

--- nanomd/scripts/m6a.sh
+++ nanomd/scripts/m6a.sh
@@ -11,4 +11,13 @@ cat Treat.mod.bed | awk -F "\t" '$4 > 0.95 {file="Treat/" $7 ".bed"; print $0 > 
 
 cat Control.mod.bed | awk -F "\t" '$4 > 0.95 {file="Control/" $7 ".bed"; print $0 > file}'
 
+cat Treat.mod.bed | awk -F "\t" '$4 >= 0.9 {file="treat_" $7 ".bed"; print $0 > file}'
+
+cat Control.mod.bed | awk -F "\t" '$4 >= 0.9 {file="control_" $7 ".bed"; print $0 > file}'
+
+awk '!seen[$4]++ {first[$4]=$0} {count[$4]++} END {for (key in count) print first[key], count[key]}' treat_m6A.bed > treat_m6A.uniq.bed
+
+nohup python ./split.py -i Treat.mod.bed -p treat -v 0.9 &
+nohup python ./split.py -i WT.mod.bed -p wt -v 0.9 &
+
 R CMD INSTALL legendBaseModel_0.0.6.tar.gz
---
+++ nanomd/utils/split.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+__author__ = "legendzdy@dingtalk.com"
+"""
+Author: legendzdy@dingtalk.com
+Data: 20250217
+Description:
+function map.
+"""
+import argparse
+
+def split_mod(input, prefix, pvalue):
+    """
+    split modification bed file into single bed files.
+    Args:
+        input: input modification bed file.
+        prefix: output file prefix.
+    Returns:
+        None.
+    """
+    
+    with open(input, 'r') as file:
+        dupfile = {}
+        for line in file:
+            count = 1
+            fields = line.strip().split("\t")
+            if float(fields[3]) >= pvalue:
+                continue
+            if dupfile.get(fields[4]) is None:
+                dupfile[fields[4]] = fields + [str(count)]
+            else:
+                count += int(dupfile[fields[4]][-1])
+                dupfile[fields[4]][-1] = str(count)
+    
+        for key, values in dupfile.items():
+            output_filename = prefix + "_" + values[6] + ".bed" 
+            with open(output_filename, 'a') as output_file:
+                output_file.write("\t".join(values) + "\n")
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Parallel modifacts detection from raw fastq data.'
+    )
+    parser.add_argument('--version', action='version', version='modkit 1.0')
+    parser.add_argument('-i', '--input', required=True)
+    parser.add_argument('-p', '--prefix', required=True)
+    parser.add_argument('-v', '--pvalue', required=True, type=float)
+    args = parser.parse_args()
+    split_mod(args.input, args.prefix, args.pvalue)
