d1e7777fb4c1a32ec0c4d0a1eda956dccd9ed4a5
DeYing Zhang <legendzdy@dingtalk.com>
2025-06-27 14:11:58 +0800

    feat: polyA

--- nanomd/modules/polyA.py
+++ nanomd/modules/polyA.py
@@ -11,8 +11,8 @@ app = typer.Typer()
 
 @app.command()
 def polyA(
-    inputs: Annotated[str, typer.Option("--inputs", "-i", help="Dir path of pod5 files.")],
-    output: Annotated[str, typer.Option("--output", "-o", help="Output file path.")]=".",
+    inputs: Annotated[Path, typer.Option("--inputs", "-i", help="Dir path of pod5 files.")],
+    output: Annotated[Path, typer.Option("--output", "-o", help="Output file path.")]=".",
     prefix: Annotated[str, typer.Option("--prefix", "-p", help="Prefix for output files.")]="prefix",
     ):
     """
